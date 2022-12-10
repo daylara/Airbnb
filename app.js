ed534a345124bd79ff76d7f120d9ff58c8dfd573
idil21 <102162076+idil21@users.noreply.github.com>
2022-12-10 20:52:23 +0300

    sign up service

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -34,15 +35,49 @@ app.get("/", async (req, res) => {
         else
         console.log(err);
     })
-    });
+});
     
 app.get("/ilan/:id", (req, res) => {
     conn.query(`SELECT description,address FROM adverts ` )
     res.render("advert");
-    });
+});
 app.get("/evsahibi", (req, res) => {
     res.render("evsahibi")
+});
+
+
+app.post("/createUser" , async (req,res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
+    const phone_number =req.body.phone_number;
+
+    const sqlSearch = "SELECT * FROM users WHERE name = ?";
+    const search_query = mysql.format(sqlSearch,[name]);
+
+    const sqlInsert = "INSERT INTO users (name , email, password , phone_number) VALUES (?,?,?,?)";
+    const insert_query =mysql.format(sqlInsert,[name,email,password,phone_number]);
+
+    conn.query(search_query , async(err,result) => {
+        if(err) throw (err);
+        console.log("------> Search Results");
+        console.log(result.length);
+
+        if(result.length !=0){
+            console.log("------> User already exists");
+            res.sendStatus(409) ;
+
+        }
+        else{
+            conn.query(insert_query,(err,result)=> {
+                if(err) throw (err);
+                console.log(result.insertId);
+                res.sendStatus(201);
+            });
+        };
     });
 
-require("./routes/advert.routes.js")(app);
+});
+
+//require("./routes/advert.routes.js")(app);
 app.listen(process.env.PORT, () => console.log("app is running"));
