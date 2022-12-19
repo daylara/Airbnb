cc0cc85053dcc62b77ce860945e427aac1ac1228
idil21 <102162076+idil21@users.noreply.github.com>
2022-12-19 18:01:57 +0300

    delete unnecessary  folders

--- routes/advert.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const adverts = require("../controllers/advert.controller.js");
-    var router = require("express").Router();
-    router.post("/", adverts.create);
-    router.get("/status", adverts.findAllStatus);
-
-    app.use('/api/adverts', router);
-};
